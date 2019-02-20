1b75f65dd606fb131202cd00b7642f30c7775e5f
Alex Zissis <alex_z9@outlook.com>
2019-02-21 01:41:23 +1100

    converted timeline into keystone objects

---
+++ models/Portfolio.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Portfolio = new keystone.List('Portfolio');
+Portfolio.add({
+    title: { type: Types.Text, initial: true, required: true },
+    description: {
+        type: Types.Textarea, initial: true, required: true
+    },
+    featured_photo: { type: Types.CloudinaryImage, initial: true, required: true },
+    photos: { type: Types.CloudinaryImages, initial: true, required: false },
+    website_demo_url: { type: Types.Url, initial: true, required: false },
+    website_demo_username: { type: Types.Text, initial: true, required: false },
+    website_demo_password: { type: Types.Text, initial: true, required: false }
+
+});
+Portfolio.register();
--- routes/views/index.js
+++ routes/views/index.js
@@ -4,7 +4,33 @@ exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	locals.hamburgerClicked = false;
+
+	locals.data = {
+		portfolios: [],
+		milestones: [],
+	};
+
+	view.on('init', function (next) {
+		keystone.list('Portfolio').model.find().sort('Title').exec(function (err, results) {
+			if (err || !results.length) {
+				return next(err);
+			}
+
+			locals.data.portfolios = results;
+			next();
+		});
+	});
+
+	view.on('init', function (next) {
+		keystone.list('Milestone').model.find().sort('Order').exec(function (err, results) {
+			if (err || !results.length) {
+				return next(err);
+			}
+
+			locals.data.milestones = results;
+			next();
+		});
+	});
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
