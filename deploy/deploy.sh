80af3fe33f44818718e2ea6f5091114da4e34cb7
Alex Zissis <alex@alexzissis.tech>
2019-05-27 20:09:52 +1000

    added travis

---
+++ deploy/deploy.sh
@@ -0,0 +1,16 @@
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+cat $ssh_key > .travis/id_rsa
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh centos@$HOST -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  git fetch --all
+  git checkout $TRAVIS_BRANCH
+  git pull
+
+  npm install
+
+  pm2 restart keystone.js
+EOF
