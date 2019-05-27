fa5cad2b726506b7850fd0a9aac754299b43ab6d
Alex Zissis <alex@alexzissis.tech>
2019-05-27 22:18:01 +1000

    ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,12 @@
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 mkdir .travis
 echo $ssh_key | openssl base64 -d > .travis/id_rsa
+wc -l .travis/id_rsa
 cat .travis/id_rsa
 chmod 600 .travis/id_rsa 
-echo $SSH_PASS | ssh-add -p .travis/id_rsa
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -o "StrictHostKeyChecking no" alex@$HOST <<EOF
+ssh -o "StrictHostKeyChecking no" -i .travis/id_rsa alex@$HOST <<EOF
   cd $DEPLOY_DIR
   git fetch --all
   git checkout $TRAVIS_BRANCH
