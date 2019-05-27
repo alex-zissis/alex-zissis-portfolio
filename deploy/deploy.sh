d1e6b2fb6ed072904fd7589415cb7e57b92d1db8
Alex Zissis <alex@alexzissis.tech>
2019-05-27 20:45:38 +1000

    ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 mkdir .travis
 openssl base64 -d <<< $ssh_key > .travis/id_rsa
 chmod 600 .travis/id_rsa # Allow read access to the private key
-
+export SSH_ASKPASS=""
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" -i .travis/id_rsa centos@$HOST -p $PORT <<EOF
   cd $DEPLOY_DIR
